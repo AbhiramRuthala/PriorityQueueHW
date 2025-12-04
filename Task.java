6abcb77bc6884d8d85873dbd52c78821056546df
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2025-12-03 23:07:13 -0500

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,63 +1,157 @@
+package pq;
+
 // Name: Abhiram Ruthala
 // Computing ID: kas4kj@virginia.edu
 // Homework Name: HW-12 Priority Queue
 // Resources used: Claude Sonnet 4.5 for debugging
 
-import java.util.Objects;
+import java.awt.*;
+import java.util.spi.AbstractResourceBundleProvider;
 
-public class Task implements Comparable<Task> {
-    private int priority;
-    private String name;
-    public Task(String name) {
-        name = "1";
-    }
-    public Task(String name, int priority) {
-        this.name = name;
-        this.priority = priority;
-    }
+@SuppressWarnings("unchecked")
+public class MyPriorityQueue<T extends Comparable<T>> implements SimplePQ<T> {
 
-    public String getName() {
-        return name;
-    }
+    //private Object[] sense;
+    private T[] heap;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
 
-    public void setName(String name) {
-        this.name = name;
+    @SuppressWarnings("unchecked")
+    public MyPriorityQueue() {
+        //sense = new Object[capacity];
+        heap = (T[]) new Comparable[DEFAULT_CAPACITY];
+        this.size = 0;
     }
 
-    public int getPriority() {
-        return priority;
+    private int parent(int index) {
+        return (index - 1) / 2;
     }
 
-    public void setPriority(int priority) {
-        this.priority = priority;
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
     }
 
 
-    //Fix comparable function - check the logic and implement it correctly. I think sense is right, but let's actually see through the logic here.
+    private void swap(int x, int y){
+        T sense = heap[x];
+        heap[x] = heap[y];
+        heap[y] = sense;
+    }
 
-    //Check over and see which one is right in terms of the implemented logic.
+    private void bubbleUp(int index){
+        while(index > 0) {
+            int parentIndex = parent(index);
+
+            if(heap[index].compareTo(heap[parentIndex]) > 0) {
+                swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+
+    }
+
+    private void bubbleDown(int index){
+        //think through this.
+        while(leftChild(index) < size) {
+            int leftChild = leftChild(index);
+            int largeValue = leftChild;
+            int rightChild = rightChild(index);
+
+            if(rightChild < size && heap[rightChild].compareTo(heap[leftChild]) > 0) {
+                largeValue = rightChild;
+            }
+
+            if(heap[index].compareTo(heap[rightChild]) < 0) {
+                swap(index, rightChild);
+                index = rightChild;
+            } else {
+                break;
+            }
+
+        }
+    }
+
+    private void resize(){
+        T[] tempHeap = (T[]) new Comparable[heap.length * 2];
+        for(int i = 0; i < heap.length; i++){
+            tempHeap[i] = heap[i];
+        }
+        heap = tempHeap;
+    }
+
+    @Override
+    public void add(T t) {
+        if(size == heap.length){
+            resize();
+        }
+        heap[size] = t;
+        bubbleUp(size);
+
+        size++;
+    }
+
 //    @Override
-//    public int compareTo(Task t1) {
-//        return 1;
+//    public void add(Object o) {
+//
+//        if(o == null){
+//            return;
+//        }
+//
+//        T newList = (T[]) new Object(capacity+1);
+//        heap = newList;
+//        heap[heap.length-1] = o;
+//
+//        size();//increase size.
+//
+//        Object[] arr = (Object[]) sense.clone();
+//        sense = arr;
+//        sense[0] = o;
 //    }
 
-    @Override
-    public int compareTo(Task o) {
-        if(o.getPriority() > priority) return 1;
-        else if(o.getPriority() < priority) return -1;
-        else return 0;
+    public void clear() {
+        for(int i=0; i<heap.length; i++){
+            heap[i] = null;
+        }
+        size=0;
     }
-
-    @Override
-    public String toString() {
-        return super.toString();
+    public boolean contains(Object o) {
+        for(int i = 0; i < size; i++) {
+            if (heap[i].equals(o) && heap[i] != null) {
+                return true;
+            }
+        }
+        return false;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Task task = (Task) o;
-        return priority == task.priority && Objects.equals(name, task.name);
+    public T peek() {
+        if(size == 0){
+            return null;
+        }
+        return heap[0];
     }
+    public T remove() {
+        if(size == 0) {
+            return null;
+        }
+        T t1 = heap[0];
+        heap[0] = heap[size-1];
+        heap[size-1] = null;
+        size--;
 
+        if(size > 0) {
+            bubbleDown(0);
+        }
+
+        return t1;
+
+
+    }
+    public int size() {
+        return size;
+    }
 }
